b8cf5805e3251e466da5fd8ba09f6aec66f48140
Axel Vanraes <axelvanraes@gmail.com>
2023-03-13 14:07:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Create a calculator by:
  * 1. creating a <form /> with a specified id="my-calculator-id" that contains the input fields you need
- * 2. use a <output /> field for your calculated result
+ * 2. use an <output /> field for your calculated result
  * 3. add a <script/> section in the footer of the <body/> section in your html where you call makeCalculator().
  *    - The first argument is the id of your calculator form
  *    - The second argument is a callback function that receives two arguments: the input (object) and the output (text value). 
@@ -10,16 +10,16 @@
  * Example for BMI: 
  * 
  * <script>
- *    makeCalculator(
+ *    makeCalculatorV2(
  *      "my-calculator-id",
- *      function (input, output) {
+ *      function (data) {
  *        const observation = {
  *          resourceType: "Observation",
  *          code: {
  *            text: "BMI"
  *          },
  *          valueQuantity: {
- *            value: parseFloat(output.value),
+ *            value: parseFloat(data.bmi),
  *            unit: "kg/m²",
  *            code: "kg/m2",
  *            system: "http://unitsofmeasure.org"
@@ -81,6 +81,58 @@ function makeCalculator(calculatorId, callback, options = {}) {
     $("#calculator").submit()
     console.debug("> AutoSave enabled.");
     $("#calculator").on("input", function (event) {
+      event.preventDefault();
+      event.currentTarget.requestSubmit();
+    });
+  }
+}
+function getFormData(event) {
+  var formElement = event.currentTarget;
+  if (!formElement instanceof HTMLFormElement) {
+    console.warn(
+      'Invalid submissions. Submission target is not a form but a',
+      event.currentTarget,
+    );
+    return;
+  }
+  var formData = new FormData(formElement);
+
+  Array.from(formElement.elements)
+    .filter((elem) => elem instanceof HTMLOutputElement)
+    .map((elem) => formData.append(elem.name, elem.value));
+  var data = Object.fromEntries(formData.entries());
+  return data;
+}
+
+function makeCalculatorV2(calculatorId, callback, options = {}) {
+  const { autoSave } = options;
+  const selector = `form#${calculatorId}`;
+  console.debug('> Creating calculator with options:', options);
+  try {
+    var url_string = window.location.href.toLocaleLowerCase();
+    var url = new URL(url_string);
+    var params = url.searchParams;
+  } catch (err) {
+    console.debug('Issues with Parsing URL Parameters');
+  }
+  console.debug('> Prepopulating fields with params', params);
+
+  for (const [key, value] of params.entries()) {
+    $(selector + ` input[name=${key}]`).val(value);
+  }
+  $(selector).trigger('input');
+  $(selector).on('submit', function (event) {
+    event.preventDefault();
+    var data = getFormData(event);
+    var result = callback(data);
+    if (window.parent) {
+      console.debug('> Result submitted to parent.');
+      window.parent.postMessage(result, '*');
+    }
+  });
+  if (autoSave) {
+    console.debug('> AutoSave enabled.');
+    $(selector).on('input', function (event) {
       event.preventDefault();
       event.currentTarget.requestSubmit();
     });
